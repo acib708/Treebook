711d803b9a2ba7dd9b7b34cbc17fd6e94d81ffe0
Alejandro Cardenas <acib708@gmail.com>
2013-07-30 14:29:24 -0500

    Added document model for attachment support.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,9 +1,12 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content, :user_id, :document_attributes
   
   #Relationships
   belongs_to :user
-  
+  belongs_to :document
+
+  accepts_nested_attributes_for :document
+
   #Tests
   validates :content, presence: true, length: {minimum: 2}
   validates_presence_of :user_id
